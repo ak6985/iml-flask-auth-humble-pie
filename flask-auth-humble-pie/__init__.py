9b4c3cae10d8abe221321b4bfaee3733b3a596cf
Anshul Kumar <ak6985@zebra.com>
2022-02-11 11:20:15 -0500

    Update setup.py

--- flask-auth-humble-pie/__init__.py
+++ flask-auth-humble-pie/__init__.py
@@ -2,7 +2,7 @@
 from .flaskauthhumblepie import reverse
 from . import main
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 __all__ = [
     "reverse",
 ]
--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ import io
 import re
 from setuptools import setup
 
-PACKAGE_NAME = "blank"
+PACKAGE_NAME = "flask-auth-humble-pie"
 
 with io.open("%s/__init__.py" % PACKAGE_NAME, "rt", encoding="utf8") as f:
     version = re.search(r"__version__ = \"(.*?)\"", f.read()).group(1)
 
-setup(version=version)
+setup(version=version,
+name='flask-auth-humble-pie')
